8245e097e31b18b9f136e6d0308e4cc59c2e8978
xinyi <wanxyzju@163.com>
2024-01-12 15:05:23 +0100

    Implement method to read configure file

---
+++ working/get_cfg.py
@@ -0,0 +1,24 @@
+
+def get_parameters(cfg):
+
+    # Define hyper-parameters
+    parameters = {
+            'net': cfg['model']['net'],
+            'learning_rate': cfg['optimizer']['learning_rate'],
+            'decay_factor':cfg['optimizer']['decay_factor'],
+            'batch_size': cfg['dataset']['batch_size'],
+            'n_epochs': cfg['train']['epochs'],
+            'task': cfg['dataset']['task_name'],
+            'fold': cfg['dataset']['fold'],
+            'idx': cfg['config']['file_idx'],
+            } 
+    if cfg['model']['net'] == 'densenet':
+        parameters['dense_init_features'] = cfg['model']['densenet']['init_features']
+        parameters['dense_growth_rate'] = cfg['model']['densenet']['growth_rate']
+        parameters['block_config'] = cfg['model']['densenet']['block_config']
+        parameters['dropout'] = cfg['model']['densenet']['dropout_prob']
+
+    if cfg['model']['net'] == 'resnet':
+        parameters['layers'] = cfg['model']['resenet']['layers']
+
+    return parameters
