a1be77f4a624a3575f71a8c6589d84da5e7c0570
xinyi <wanxyzju@163.com>
2024-01-12 18:13:21 +0100

    chanegs file name to utilities

--- working/utilities.py
+++ working/utilities.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from neptune.types import File
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import nibabel as nib
 
 def log_confusion_matrix(lit_model, data_module, neptune_logger, cp_dir):
     lit_model.freeze()
@@ -21,3 +22,11 @@ def log_confusion_matrix(lit_model, data_module, neptune_logger, cp_dir):
     fig = ConfusionMatrixDisplay(confusion_matrix=cm)
     fig.plot().figure_.savefig(os.path.join(cp_dir,'confusion_matrix.png'))
     neptune_logger.experiment["confusion_matrix"].upload(File.as_image(fig.plot().figure_))
+
+# Todo:
+def extract_3d_bbx(f_seg):
+    seg = nib.load(f_seg).get_fdata()
+    x_min, x_max = int(np.where(seg==1)[0].min()), int(np.where(seg==1)[0].max())
+    y_min, y_max = int(np.where(seg==1)[1].min()), int(np.where(seg==1)[1].max())
+    z_min, z_max = int(np.where(seg==1)[2].min()), int(np.where(seg==1)[2].max())
+    return [x_min, x_max+1, y_min, y_max+1, z_min, z_max+1]
